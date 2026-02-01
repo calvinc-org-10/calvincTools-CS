5eaca2d9db5f17dbe9d47c7ba295fb078cd413b4
Calvin C <78990192+curtindolph-calvin-10@users.noreply.github.com>
2026-02-01 14:09:47 -0600

    Clean up config.py by removing comments and secrets
    Removed commented-out lines and sensitive information from config.py.

--- calvincTools/config.py
+++ calvincTools/config.py
@@ -1,15 +1,4 @@
-# proddbtype = "sqlite"
-root_path = "."
-# root_path = os.path.abspath(os.path.dirname(__file__))
-cMenu_dbPath = f"{root_path}\\cMenudb.sqlite"
-# cMenu_dbPath = os.path.join(root_path, 'cMenudb.sqlite')
-
-proddbtype = "mysql"
-proddbusr = "calvinc461"
-proddbpw = "tMJxBC!D4zRpU_2"
-proddbsvr = "calvinc461.mysql.pythonanywhere-services.com"
-proddbdb = "calvinc461$cMenu"    # calvinc461$default
-
+import * from .secrets
 
 calvincTools_config = {
     # 'SQLALCHEMY_BINDS': {'cToolsdb': f'{proddbtype}:///{cMenu_dbPath}'}
