e2b7f7abf6d70da4d690aab32e1ed713c3274889
Calvin C <78990192+curtindolph-calvin-10@users.noreply.github.com>
2026-02-01 13:59:30 -0600

    Update database credentials to placeholders

--- calvincTools/RENAME_TO_secrets.py
+++ calvincTools/RENAME_TO_secrets.py
@@ -1,10 +1,11 @@
+# proddbtype = "sqlite"
 root_path = "."
 # root_path = os.path.abspath(os.path.dirname(__file__))
 cMenu_dbPath = f"{root_path}\\cMenudb.sqlite"
 # cMenu_dbPath = os.path.join(root_path, 'cMenudb.sqlite')
 
 proddbtype = "mysql"
-proddbusr = "calvinc461"
-proddbpw = "tMJxBC!D4zRpU_2"
-proddbsvr = "calvinc461.mysql.pythonanywhere-services.com"
-proddbdb = "calvinc461$cMenu"    # calvinc461$default
+proddbusr = "db user name"
+proddbpw = "db password"
+proddbsvr = "db server url"
+proddbdb = "db name"
