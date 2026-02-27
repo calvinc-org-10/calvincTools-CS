9bb19f45b16ab4ace1af228570e34b447e1e2417
Calvin C <curtindolph.calvin.10@gmail.com>
2026-02-27 13:48:41 -0600

    WIP - cTools_models in init_cDatabase

--- calvincTools/models.py
+++ calvincTools/models.py
@@ -371,162 +371,174 @@ def init_cDatabase(
         # menuGroups
         cTools_models['menuGroups'] = menuGroups
 
-
-    class menuItems(_ModelInitMixin, db_instance.Model):   #pylint: disable=redefined-outer-name
-        """Menu items model with database columns."""
-        __bind_key__ = cTools_bind_key
-        __tablename__ = cTools_tablenames.get('menuItems', 'cMenu_menuItems')
-        
-        id = db_instance.Column(db_instance.Integer, primary_key=True)
-        MenuGroup_id = db_instance.Column(db_instance.Integer, db_instance.ForeignKey('cMenu_menuGroups.id', ondelete='RESTRICT'), nullable=True)
-        MenuID = db_instance.Column(db_instance.SmallInteger, nullable=False)
-        OptionNumber = db_instance.Column(db_instance.SmallInteger, nullable=False)
-        OptionText = db_instance.Column(db_instance.String(250), nullable=False)
-        Command = db_instance.Column(db_instance.Integer, nullable=True)
-        Argument = db_instance.Column(db_instance.String(250), default='')
-        pword = db_instance.Column(db_instance.String(250), default='')
-        top_line = db_instance.Column(db_instance.Boolean, nullable=True)
-        bottom_line = db_instance.Column(db_instance.Boolean, nullable=True)
-        
-        # Relationships
-        menu_group = db_instance.relationship('menuGroups', back_populates='menu_items', lazy='joined')
-        
-        # Unique constraint
-        __table_args__ = (
-            UniqueConstraint('MenuGroup_id', 'MenuID', 'OptionNumber', 
-                            name='uq_menu_group_MenuID_OptionNumber'),
-        )
-        
-        def __repr__(self):
-            return f'<MenuItem {self.MenuGroup_id},{self.MenuID}/{self.OptionNumber}>'
-        
-        def __str__(self):
-            return f'{self.menu_group}, {self.MenuID}/{self.OptionNumber}, {self.OptionText}'
-
-        def __init__(self, **kw: Any):
-            """Initialize a new menuItems instance."""
-            inspector = inspect(db_instance.engine)
-            if not inspector.has_table(self.__tablename__):
-                # If the table does not exist, create it
-                db_instance.create_all()
-            super().__init__(**kw)
-
-    class cParameters(_ModelInitMixin, db_instance.Model):   #pylint: disable=redefined-outer-name
-        """Parameters model with database columns."""
-        __bind_key__ = cTools_bind_key
-        __tablename__ = cTools_tablenames.get('cParameters', 'cMenu_cParameters')
-        
-        parm_name: str = db_instance.Column(db_instance.String(100), primary_key=True)
-        parm_value: str = db_instance.Column(db_instance.String(512), default='', nullable=False)
-        user_modifiable: bool = db_instance.Column(db_instance.Boolean, default=True, nullable=False)
-        comments: str = db_instance.Column(db_instance.String(512), default='', nullable=False)
-        
-        def __repr__(self):
-            return f'<Parameter {self.parm_name}>'
-        
-        def __str__(self):
-            return f'{self.parm_name} ({self.parm_value})'
-        
-        @classmethod
-        def get_parameter(cls, parm_name: str, default: str = '') -> str:
-            """Django equivalent: getcParm"""
-            param = cls.query.filter_by(parm_name=parm_name).first()
-            return param.parm_value if param else default
-        
-        @classmethod
-        def set_parameter(cls, parm_name: str, parm_value: str, user_modifiable: bool = True, comments: str = ''):
-            """Django equivalent: setcParm"""
-            param = cls.query.filter_by(parm_name=parm_name).first()
-            if param:
-                param.parm_value = parm_value
-            else:
-                param = cls(
-                    parm_name=parm_name,
-                    parm_value=parm_value,
-                    user_modifiable=user_modifiable,
-                    comments=comments
-                )
-                db_instance.session.add(param)
+    if cTools_models.get('menuItems') is None:
+        class menuItems(_ModelInitMixin, db_instance.Model):   #pylint: disable=redefined-outer-name
+            """Menu items model with database columns."""
+            __bind_key__ = cTools_bind_key
+            __tablename__ = cTools_tablenames.get('menuItems', 'cMenu_menuItems')
             
-            db_instance.session.commit()
-            return param
+            id = db_instance.Column(db_instance.Integer, primary_key=True)
+            MenuGroup_id = db_instance.Column(db_instance.Integer, db_instance.ForeignKey('cMenu_menuGroups.id', ondelete='RESTRICT'), nullable=True)
+            MenuID = db_instance.Column(db_instance.SmallInteger, nullable=False)
+            OptionNumber = db_instance.Column(db_instance.SmallInteger, nullable=False)
+            OptionText = db_instance.Column(db_instance.String(250), nullable=False)
+            Command = db_instance.Column(db_instance.Integer, nullable=True)
+            Argument = db_instance.Column(db_instance.String(250), default='')
+            pword = db_instance.Column(db_instance.String(250), default='')
+            top_line = db_instance.Column(db_instance.Boolean, nullable=True)
+            bottom_line = db_instance.Column(db_instance.Boolean, nullable=True)
+            
+            # Relationships
+            menu_group = db_instance.relationship('menuGroups', back_populates='menu_items', lazy='joined')
+            
+            # Unique constraint
+            __table_args__ = (
+                UniqueConstraint('MenuGroup_id', 'MenuID', 'OptionNumber', 
+                                name='uq_menu_group_MenuID_OptionNumber'),
+            )
+            
+            def __repr__(self):
+                return f'<MenuItem {self.MenuGroup_id},{self.MenuID}/{self.OptionNumber}>'
+            
+            def __str__(self):
+                return f'{self.menu_group}, {self.MenuID}/{self.OptionNumber}, {self.OptionText}'
 
-    class cGreetings(_ModelInitMixin, db_instance.Model):   #pylint: disable=redefined-outer-name
-        """Greetings model with database columns."""
-        __bind_key__ = cTools_bind_key
-        __tablename__ = cTools_tablenames.get('cGreetings', 'cMenu_cGreetings')
-        
-        id = db_instance.Column(db_instance.Integer, primary_key=True)
-        greeting = db_instance.Column(db_instance.String(2000), nullable=False)
-        
-        def __repr__(self):
-            return f'<Greeting {self.id}>'
-        
-        def __str__(self):
-            return f'{self.greeting} (ID: {self.id})'
+            def __init__(self, **kw: Any):
+                """Initialize a new menuItems instance."""
+                inspector = inspect(db_instance.engine)
+                if not inspector.has_table(self.__tablename__):
+                    # If the table does not exist, create it
+                    db_instance.create_all()
+                super().__init__(**kw)
+        # menuItems
+        cTools_models['menuItems'] = menuItems
 
-    class User(UserMixin, db_instance.Model):   #pylint: disable=redefined-outer-name
-        """
-        User model for authentication with database columns.
-        Inherit from UserMixin to get default implementations for:
-        - is_authenticated, is_active, is_anonymous, get_id()
-        """
-        __bind_key__ =  cTools_bind_key
-        __tablename__ = cTools_tablenames.get('User', 'users')
+    if cTools_models.get('cParameters') is None:
+        class cParameters(_ModelInitMixin, db_instance.Model):   #pylint: disable=redefined-outer-name
+            """Parameters model with database columns."""
+            __bind_key__ = cTools_bind_key
+            __tablename__ = cTools_tablenames.get('cParameters', 'cMenu_cParameters')
+            
+            parm_name: str = db_instance.Column(db_instance.String(100), primary_key=True)
+            parm_value: str = db_instance.Column(db_instance.String(512), default='', nullable=False)
+            user_modifiable: bool = db_instance.Column(db_instance.Boolean, default=True, nullable=False)
+            comments: str = db_instance.Column(db_instance.String(512), default='', nullable=False)
+            
+            def __repr__(self):
+                return f'<Parameter {self.parm_name}>'
+            
+            def __str__(self):
+                return f'{self.parm_name} ({self.parm_value})'
+            
+            @classmethod
+            def get_parameter(cls, parm_name: str, default: str = '') -> str:
+                """Django equivalent: getcParm"""
+                param = cls.query.filter_by(parm_name=parm_name).first()
+                return param.parm_value if param else default
+            
+            @classmethod
+            def set_parameter(cls, parm_name: str, parm_value: str, user_modifiable: bool = True, comments: str = ''):
+                """Django equivalent: setcParm"""
+                param = cls.query.filter_by(parm_name=parm_name).first()
+                if param:
+                    param.parm_value = parm_value
+                else:
+                    param = cls(
+                        parm_name=parm_name,
+                        parm_value=parm_value,
+                        user_modifiable=user_modifiable,
+                        comments=comments
+                    )
+                    db_instance.session.add(param)
+                
+                db_instance.session.commit()
+                return param
+            # set_parameter
+        # cParameters
+        cTools_models['cParameters'] = cParameters
 
-        id = db_instance.Column(db_instance.Integer, primary_key=True)
-        username = db_instance.Column(db_instance.String(80), unique=True, nullable=False, index=True)
-        email = db_instance.Column(db_instance.String(120), unique=True, nullable=False, index=True)
-        password_hash = db_instance.Column(db_instance.String(255), nullable=False)
-        FLDis_active = db_instance.Column(db_instance.Boolean, default=True, nullable=False)
-        is_superuser = db_instance.Column(db_instance.Boolean, default=False, nullable=False)
-        permissions = db_instance.Column(db_instance.String(1024), nullable=False, default='')
-        menuGroup = db_instance.Column(db_instance.Integer, db_instance.ForeignKey(menuGroups.id), nullable=True)
-        date_joined = db_instance.Column(db_instance.DateTime, default=datetime.now, nullable=False)
-        last_login = db_instance.Column(db_instance.DateTime, nullable=True)
+    if cTools_models.get('cGreetings') is None:
+        class cGreetings(_ModelInitMixin, db_instance.Model):   #pylint: disable=redefined-outer-name
+            """Greetings model with database columns."""
+            __bind_key__ = cTools_bind_key
+            __tablename__ = cTools_tablenames.get('cGreetings', 'cMenu_cGreetings')
+            
+            id = db_instance.Column(db_instance.Integer, primary_key=True)
+            greeting = db_instance.Column(db_instance.String(2000), nullable=False)
+            
+            def __repr__(self):
+                return f'<Greeting {self.id}>'
+            
+            def __str__(self):
+                return f'{self.greeting} (ID: {self.id})'
+        # cGreetings
+        cTools_models['cGreetings'] = cGreetings
 
-        @property
-        def is_active(self):
-            return self.FLDis_active
-        
-        def set_password(self, password):
-            """Hash and set the user's password."""
-            self.password_hash = generate_password_hash(password)
+    if cTools_models.get('User') is None:
+        class User(UserMixin, db_instance.Model):   #pylint: disable=redefined-outer-name
+            """
+            User model for authentication with database columns.
+            Inherit from UserMixin to get default implementations for:
+            - is_authenticated, is_active, is_anonymous, get_id()
+            """
+            __bind_key__ =  cTools_bind_key
+            __tablename__ = cTools_tablenames.get('User', 'users')
 
-        def check_password(self, password):
-            """Check if the provided password matches the hash."""
-            verdict = check_password_hash(self.password_hash, password)
-            return verdict
+            id = db_instance.Column(db_instance.Integer, primary_key=True)
+            username = db_instance.Column(db_instance.String(80), unique=True, nullable=False, index=True)
+            email = db_instance.Column(db_instance.String(120), unique=True, nullable=False, index=True)
+            password_hash = db_instance.Column(db_instance.String(255), nullable=False)
+            FLDis_active = db_instance.Column(db_instance.Boolean, default=True, nullable=False)
+            is_superuser = db_instance.Column(db_instance.Boolean, default=False, nullable=False)
+            permissions = db_instance.Column(db_instance.String(1024), nullable=False, default='')
+            menuGroup = db_instance.Column(db_instance.Integer, db_instance.ForeignKey(menuGroups.id), nullable=True)
+            date_joined = db_instance.Column(db_instance.DateTime, default=datetime.now, nullable=False)
+            last_login = db_instance.Column(db_instance.DateTime, nullable=True)
 
-        def has_permission(self, permission_name: str) -> bool:
-            """Check if the user has a specific permission."""
-            if self.is_superuser:
-                return True  # Superusers have all permissions
-            permissions_list = self.permissions.lower().split(',') if self.permissions else []
-            return permission_name.lower() in permissions_list
+            @property
+            def is_active(self):
+                return self.FLDis_active
+            
+            def set_password(self, password):
+                """Hash and set the user's password."""
+                self.password_hash = generate_password_hash(password)
 
-        def update_last_login(self):
-            """Update the last login timestamp."""
-            self.last_login = datetime.now()
-            db_instance.session.commit()
+            def check_password(self, password):
+                """Check if the provided password matches the hash."""
+                verdict = check_password_hash(self.password_hash, password)
+                return verdict
 
-        def __repr__(self):
-            return f'<User {self.username}>'
+            def has_permission(self, permission_name: str) -> bool:
+                """Check if the user has a specific permission."""
+                if self.is_superuser:
+                    return True  # Superusers have all permissions
+                permissions_list = self.permissions.lower().split(',') if self.permissions else []
+                return permission_name.lower() in permissions_list
 
-        def __init__(self, **kw: Any):
-            """Initialize a user instance."""
-            inspector = inspect(db_instance.engine)
-            if not inspector.has_table(self.__tablename__):
-                # If the table does not exist, create it
-                db_instance.create_all()
-            super().__init__(**kw)
+            def update_last_login(self):
+                """Update the last login timestamp."""
+                self.last_login = datetime.now()
+                db_instance.session.commit()
+
+            def __repr__(self):
+                return f'<User {self.username}>'
+
+            def __init__(self, **kw: Any):
+                """Initialize a user instance."""
+                inspector = inspect(db_instance.engine)
+                if not inspector.has_table(self.__tablename__):
+                    # If the table does not exist, create it
+                    db_instance.create_all()
+                super().__init__(**kw)
+        # User
+        cTools_models['User'] = User
 
     # Update the module-level references so imports work
-    setattr(current_module, 'menuGroups', menuGroups)
-    setattr(current_module, 'menuItems', menuItems)
-    setattr(current_module, 'cParameters', cParameters)
-    setattr(current_module, 'cGreetings', cGreetings)
-    setattr(current_module, 'User', User)
+    setattr(current_module, 'menuGroups', cTools_models['menuGroups'])
+    setattr(current_module, 'menuItems', cTools_models['menuItems'])
+    setattr(current_module, 'cParameters', cTools_models['cParameters'])
+    setattr(current_module, 'cGreetings', cTools_models['cGreetings'])
+    setattr(current_module, 'User', cTools_models['User'])
     
     # Create all tables in the database
     with flskapp.app_context():
